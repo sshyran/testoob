23f181b63bc94c9c51ab9d31aceaa4426dad20db
orip <orip@c019955c-93d0-11dd-bc7b-59c4a08ba726>
2005-08-22 22:58:14 +0000

    Branching release 0.6

---
+++ branches/RB-0.6/tests/helpers.py
@@ -0,0 +1,71 @@
+def project_subpath(*components):
+    from os.path import dirname, join, normpath
+    return normpath(join(dirname(__file__), "..", *components))
+
+def module_path():
+    return project_subpath("src")
+
+def executable_path():
+    return project_subpath("src", "testoob", "testoob")
+
+def fix_include_path():
+    import sys
+    if module_path() not in sys.path:
+        sys.path.insert(0, module_path())
+
+fix_include_path()
+
+class ReporterWithMemory:
+    "A reporter that remembers info on the test fixtures run"
+    def __init__(self):
+        self.started = []
+        self.successes = []
+        self.errors = []
+        self.failures = []
+        self.finished = []
+        self.stdout = ""
+        self.stderr = ""
+
+        self.has_started = False
+        self.is_done = False
+
+    def _append_test(self, l, test):
+        l.append(str(test).split()[0])
+
+    def start(self):
+        self.has_started = True
+    def done(self):
+        self.is_done = True
+    def startTest(self, test):
+        self._append_test(self.started, test)
+    def stopTest(self, test):
+        self._append_test(self.finished, test)
+    def addError(self, test, err):
+        self._append_test(self.errors, test)
+    def addFailure(self, test, err):
+        self._append_test(self.failures, test)
+    def addSuccess(self, test):
+        self._append_test(self.successes, test)
+
+    def __str__(self):
+        attrs = ("started","successes","errors","failures","finished","stdout","stderr")
+        return "\n".join(["%s = %s" % (attr, getattr(self,attr)) for attr in attrs])
+
+import unittest
+import testoob.running
+class TestoobBaseTestCase(unittest.TestCase):
+    def setUp(self):
+        self.reporter = ReporterWithMemory()
+    def tearDown(self):
+        del self.reporter
+    def _check_reporter(self, **kwargs):
+        for attr, expected in kwargs.items():
+            actual = getattr(self.reporter, attr)
+            if type(expected) == type([]):
+                expected.sort()
+                actual.sort()
+            self.assertEqual(expected, actual)
+
+    def _run(self, **kwargs):
+        testoob.running.run(reporters=[self.reporter], **kwargs)
+
---
+++ branches/RB-0.6/tests/systemtests.py
@@ -0,0 +1,187 @@
+import helpers
+helpers.fix_include_path()
+
+import unittest, testoob
+
+_suite_file = helpers.project_subpath("tests/suites.py")
+
+def _create_args(tests=None, options=None):
+    import sys
+    result = [sys.executable, helpers.executable_path(), _suite_file]
+    if options is not None: result += options
+    if tests is not None: result += tests
+    return result
+
+class CommandLineTestCase(unittest.TestCase):
+    def testSuccesfulRunNormal(self):
+        args = _create_args(options=[], tests=["CaseDigits"])
+        regex = r"""
+\.\.\.\.\.\.\.\.\.\.
+----------------------------------------------------------------------
+Ran 10 tests in \d\.\d+s
+OK
+""".strip()
+
+        testoob.testing.command_line(args=args, expected_error_regex=regex)
+
+    def testSuccesfulRunQuiet(self):
+        args = _create_args(options=["-q"], tests=["CaseDigits"])
+        regex = r"""
+^----------------------------------------------------------------------
+Ran 10 tests in \d\.\d+s
+OK$
+""".strip()
+
+        testoob.testing.command_line(args=args, expected_error_regex=regex)
+
+    def testSuccesfulRunVerbose(self):
+        args = _create_args(options=["-v"], tests=["CaseDigits"])
+        regex = r"""
+test0 \(.*suites\.CaseDigits\.test0\) \.\.\. ok
+test1 \(.*suites\.CaseDigits\.test1\) \.\.\. ok
+test2 \(.*suites\.CaseDigits\.test2\) \.\.\. ok
+test3 \(.*suites\.CaseDigits\.test3\) \.\.\. ok
+test4 \(.*suites\.CaseDigits\.test4\) \.\.\. ok
+test5 \(.*suites\.CaseDigits\.test5\) \.\.\. ok
+test6 \(.*suites\.CaseDigits\.test6\) \.\.\. ok
+test7 \(.*suites\.CaseDigits\.test7\) \.\.\. ok
+test8 \(.*suites\.CaseDigits\.test8\) \.\.\. ok
+test9 \(.*suites\.CaseDigits\.test9\) \.\.\. ok
+
+----------------------------------------------------------------------
+Ran 10 tests in \d\.\d+s
+OK
+""".strip()
+
+        testoob.testing.command_line(args=args, expected_error_regex=regex)
+
+    def testFailureRunQuiet(self):
+        args = _create_args(options=["-q"], tests=["CaseFailure"])
+        regex = r"""
+^======================================================================
+FAIL: testFailure \(.*suites\.CaseFailure\.testFailure\)
+----------------------------------------------------------------------
+.*AssertionError
+
+----------------------------------------------------------------------
+Ran 1 test in \d\.\d+s
+FAILED \(failures=1\)$
+""".strip()
+
+        testoob.testing.command_line(args=args, expected_error_regex=regex)
+
+    def testRegex(self):
+        args = _create_args(options=["-v", "--regex=A|D|J"], tests=["CaseLetters"])
+        regex=r"""
+testA \(.*suites\.CaseLetters\.testA\) \.\.\. ok
+testD \(.*suites\.CaseLetters\.testD\) \.\.\. ok
+testJ \(.*suites\.CaseLetters\.testJ\) \.\.\. ok
+""".strip()
+
+        testoob.testing.command_line(args=args, expected_error_regex=regex)
+
+    def testVassertSimple(self):
+        args = _create_args(options=["--regex=CaseDigits.test0", "--vassert"])
+        regex = r"""
+test0 \(suites\.CaseDigits\.test0\) \.\.\. ok
+  \[ PASSED \(assertEquals\) first: "00" second: "00" \]
+
+----------------------------------------------------------------------
+""".strip()
+        testoob.testing.command_line(args=args, expected_error_regex=regex)
+
+    def testVassertFormatStrings(self):
+        args = _create_args(options=["--regex=MoreTests.test.*FormatString",
+                                     "--vassert"])
+        regex = r"""
+test.*FormatString \(suites\.MoreTests\.test.*FormatString\) \.\.\. ok
+  \[ PASSED \(assertEquals\) first: "%s" second: "%s" \]
+
+----------------------------------------------------------------------
+""".strip()
+        testoob.testing.command_line(args=args, expected_error_regex=regex)
+
+    def testImmediateReporting(self):
+        args = _create_args(tests=["CaseMixed.testFailure", "CaseMixed.testSuccess"],
+                            options=["-v", "--immediate"])
+        # Check that the fail message appears before testSuccess is run
+        regex = "testFailure.*FAIL.*FAIL: testFailure.*Traceback.*testSuccess.*ok"
+        testoob.testing.command_line(args=args, expected_error_regex=regex)
+
+    def testLaunchingPdb(self):
+        args = _create_args(tests=["CaseMixed.testFailure"], options=["--debug"])
+        output_regex="Debugging for failure in test: testFailure \(suites\.CaseMixed\.testFailure\).*raise self\.failureException, msg.*\(Pdb\)"
+        error_regex="" # accept anything on the standard error
+        testoob.testing.command_line(args=args,
+                                     expected_output_regex=output_regex,
+                                     expected_error_regex=error_regex)
+
+    def testXMLReporting(self):
+        import tempfile
+        xmlfile = tempfile.mktemp(".testoob-testXMLReporting")
+        args = _create_args(options=["--xml=" + xmlfile], tests=["CaseMixed"])
+
+        try:
+            testoob.testing._run_command(args)
+            from elementtree.ElementTree import parse
+            root = parse(xmlfile).getroot()
+
+            # testsuites tag
+            self.assertEqual("testsuites", root.tag)
+
+            def extract_info(testcase):
+                class Struct: pass
+                result = Struct()
+                result.tag = testcase.tag
+                result.name = testcase.attrib["name"]
+                result.result = testcase.find("result").text
+                failure = testcase.find("failure")
+                result.failure = failure is not None and failure.attrib["type"] or None
+                error = testcase.find("error")
+                result.error = error is not None and error.attrib["type"] or None
+                return result
+
+            testcase_reports = [extract_info(testcase) for testcase in root.findall("testcase")]
+
+            # ensure one testcase of each type
+            [success] = [x for x in testcase_reports if x.result == "success"]
+            [failure] = [x for x in testcase_reports if x.result == "failure"]
+            [error]   = [x for x in testcase_reports if x.result == "error"]
+
+            def check_result(testcase, name=None, failure=None, error=None):
+                self.assertEqual("testcase", testcase.tag)
+                self.assertEqual(name, testcase.name)
+                self.assertEqual(failure, testcase.failure)
+                self.assertEqual(error, testcase.error)
+
+            check_result(success, name="testSuccess (suites.CaseMixed)")
+            check_result(failure, name="testFailure (suites.CaseMixed)",
+                                  failure="exceptions.AssertionError")
+            check_result(error,   name="testError (suites.CaseMixed)",
+                                  error="exceptions.RuntimeError")
+
+        finally:
+            import os
+            os.unlink(xmlfile)
+
+    def testHTMLReporting(self):
+        import tempfile
+        htmlfile = tempfile.mktemp(".testoob-testHTMLReporting")
+        args = _create_args(options=["--html=" + htmlfile], tests=["CaseMixed"])
+
+        try:
+            testoob.testing._run_command(args)
+            htmlcontents = open(htmlfile).read()
+
+            from testoob.testing import assert_matches
+            assert htmlcontents.find("<title>TestOOB report</title>") >= 0
+            assert htmlcontents.find("def testError(self): raise RuntimeError") >= 0
+            assert htmlcontents.find("def testFailure(self): self.fail()") >= 0
+
+            assert_matches(r"testSuccess.*>Success<", htmlcontents)
+        finally:
+            import os
+            os.unlink(htmlfile)
+
+def suite(): return unittest.makeSuite(CommandLineTestCase)
+if __name__ == "__main__": unittest.main()
