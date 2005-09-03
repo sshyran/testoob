a862f16c6005facd3c82c202784a5392fabe1ab1
misha <misha@c019955c-93d0-11dd-bc7b-59c4a08ba726>
2005-09-03 11:25:36 +0000

    First full implemintations, still to do: refuctor, make catches of errors and failures (pickle problem)

--- branches/TICKET-37/src/testoob/processed_helper.py
+++ branches/TICKET-37/src/testoob/processed_helper.py
@@ -13,29 +13,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License. 
 
-class ProcessedRunnerHelper():
+class ProcessedRunnerHelper:
     "A helper class to make ProcessedRunner shorter and clearer."
     def __init__(self, max_processes):
-        self._fixturesList = [[]] * max_processes
+        self._fixturesList = [[] for i in xrange(max_processes)]
         self._load_balance_idx = 0
 
     def register_fixture(self, fixture):
-        self._fixturesList[self._load_balance_idx].append(value)
+        self._fixturesList[self._load_balance_idx].append(fixture)
         self._load_balance_idx = (self._load_balance_idx + 1) % len(self._fixturesList)
 
     def start(self, reporter):
-        from os import fork as _os_fork, pipe as _os_pipe, fdopen as _os_fdopen
-        from sys import exit as _sys_exit
+        from os import fork, pipe, fdopen
+        from sys import exit
 
         children = []
 
         for processFixtures in self._fixturesList:
-            pipePair = map(_os_fdopen, _os_pipe())
-            pid = _os_fork()
+            pipePair = [(fdopen(r, 'r'), fdopen(w, 'w')) for r, w in [pipe()]][0]
+            pid = fork()
             if pid == 0:
+                pipePair[0].close()
                 pipeReporter = self._create_pipeReporter(pipePair[1])
                 self._run_fixtures(processFixtures, pipeReporter)
-                _sys_exit()
+                pipePair[1].close()
+                exit()
+            pipePair[1].close()
             children.append({"pid": pid, "pipe": pipePair[0]})
 
         self._listen(reporter, children)
@@ -43,24 +46,28 @@ class ProcessedRunnerHelper():
     def _run_fixtures(self, fixtures, reporter):
         [fixture(reporter) for fixture in fixtures]
 
-    def _create_pipeReporter(self, writeFD):
-        # TODO: implement.
-        pass
+    def _create_pipeReporter(self, writePipe):
+        from reporting import ProcessedReporter
+        # TODO: better implemintation.
+        return ProcessedReporter(writePipe)
 
     def _listen(self, reporter, children):
         from base64 import decodestring
         from cPickle import loads
         from os import waitpid
+        from select import select
         while children:
-            for child in children:
-                if child["pipe"].closed(): # TODO implement rightly.
+            ready = select([child["pipe"] for child in children], [], [])[0]
+            for childsPipe in ready:
+                received = childsPipe.readline().split(" ")
+                if received == ['']:
+                    child = [child for child in children if child["pipe"] == childsPipe][0]
                     waitpid(child["pid"], 0)
                     children.remove(child)
-                elif child["pipe"].can_read(): # TODO implement rightly.
-                    received = child["pipe"].readline().split(" ")
-                    received[1:] = map(lambda x: loads(decodestring(x)), received[1:])
-                    try:
-                        getattr(reporter, received[0])(*received[1:])
-                    except Exception, e:
-                        pass # TODO: do something here...
+                    continue
+                received[1:] = map(lambda x: loads(decodestring(eval(x))), received[1:])
+                try:
+                    getattr(reporter, received[0])(*received[1:])
+                except Exception, e:
+                    pass # TODO: do something here...
 
