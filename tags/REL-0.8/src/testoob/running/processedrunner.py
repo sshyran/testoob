f8d8214987b92c53e9d49c8146437bc5d9245d4c
orip <orip@c019955c-93d0-11dd-bc7b-59c4a08ba726>
2005-11-30 00:23:41 +0000

    Tagging release 0.8

---
+++ tags/REL-0.8/src/testoob/running/processedrunner.py
@@ -0,0 +1,31 @@
+# TestOOB, Python Testing Out Of (The) Box
+# Copyright (C) 2005 The TestOOB Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from baserunner import BaseRunner
+
+class ProcessedRunner(BaseRunner):
+    "Run tests using fork in different processes."
+    def __init__(self, max_processes=1):
+        from processed_helper import ProcessedRunnerHelper
+        BaseRunner.__init__(self)
+        self._helper = ProcessedRunnerHelper(max_processes)
+
+    def run(self, fixture):
+        BaseRunner.run(self, fixture)
+        self._helper.register_fixture(fixture)
+
+    def done(self):
+        self._helper.start(self.reporter)
+        BaseRunner.done(self)
