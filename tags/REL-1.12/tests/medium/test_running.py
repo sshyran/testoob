015c72fb99bb5154d266605709b20426cee13208
orip <orip@c019955c-93d0-11dd-bc7b-59c4a08ba726>
2006-09-09 18:37:18 +0000

    Updating 1.12 tag

---
+++ tags/REL-1.12/tests/medium/test_running.py
@@ -0,0 +1,95 @@
+import unittest
+import helpers, suites
+
+class RunningTestCase(helpers.TestoobBaseTestCase):
+    def testSuccessfulRun(self):
+        self._run(suite=suites.suite())
+        self._check_reporter(
+                started   = suites.all_test_names,
+                finished  = suites.all_test_names,
+                successes = suites.all_test_names,
+                failures  = [],
+                errors    = [],
+#                asserts   = suites.all_asserts,
+                stdout    = "",
+                stderr    = "",
+            )
+
+    def testFailureRun(self):
+        self._run(suite=suites.CaseFailure.suite())
+        self._check_reporter(
+                started   = ["testFailure"],
+                finished  = ["testFailure"],
+                successes = [],
+                failures  = ["testFailure"],
+                errors    = [],
+#                asserts   = {"testFailure": ("fail", AssertionError)},
+                stdout    = "",
+                stderr    = "",
+            )
+
+    def testErrorRun(self):
+        self._run(suite=suites.CaseError.suite())
+        self._check_reporter(
+                started   = ["testError"],
+                finished  = ["testError"],
+                successes = [],
+                failures  = [],
+                errors    = ["testError"],
+#                asserts   = {},
+                stdout    = "",
+                stderr    = "",
+            )
+
+    def testMixedRun(self):
+        self._run(suite=suites.CaseMixed.suite())
+        self._check_reporter(
+                started   = ["testError", "testSuccess", "testFailure"],
+                finished  = ["testError", "testSuccess", "testFailure"],
+                successes = ["testSuccess"],
+                failures  = ["testFailure"],
+                errors    = ["testError"],
+#                asserts   = {"testFailure": ("fail", AssertionError)},
+                stdout    = "",
+                stderr    = "",
+            )
+
+    def testEmptyRun(self):
+        self._run(suite=suites.CaseEmpty.suite())
+        self._check_reporter(
+                started   = [],
+                finished  = [],
+                successes = [],
+                failures  = [],
+                errors    = [],
+#                asserts   = {},
+                stdout    = "",
+                stderr    = "",
+            )
+
+    def testAssertRaises(self):
+        self._run(suite=suites.AssertRaisesTests.suite())
+        self._check_reporter(
+                started   = ["testAssertRaisesArgs",
+                             "testAssertRaisesArgsKwargs",
+                             "testAssertRaisesKwargs"],
+                finished  = ["testAssertRaisesArgs",
+                             "testAssertRaisesArgsKwargs",
+                             "testAssertRaisesKwargs"],
+                successes = ["testAssertRaisesArgs",
+                             "testAssertRaisesArgsKwargs",
+                             "testAssertRaisesKwargs"],
+                failures  = [],
+                errors    = [],
+#                asserts   = {
+#                    "testAssertRaisesArgs"       : ("assertRaises", None.__class__),
+#                    "testAssertRaisesArgsKwargs" : ("assertRaises", None.__class__),
+#                    "testAssertRaisesKwargs"     : ("assertRaises", None.__class__),
+#                    },
+                stdout    = "",
+                stderr    = "",
+            )
+
+if __name__ == "__main__":
+    import testoob
+    testoob.main()
